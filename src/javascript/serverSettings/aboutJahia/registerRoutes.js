9f454e84fe5f63e22a4ff38c485e6214f85e8bd9
alex <alexandrk_@hotmail.com>
2020-01-27 12:41:08 -0500

    BACKLOG-11712 Add ids

--- src/javascript/serverSettings/aboutJahia/registerRoutes.js
+++ src/javascript/serverSettings/aboutJahia/registerRoutes.js
@@ -4,11 +4,13 @@ export const registerRoutes = function () {
     const level = 'server';
     const path = '/administration/aboutJahia';
 
+    const routeId = 'aboutJahia';
     registry.add(`${level}-${path.toLowerCase()}`, {
+        id: routeId,
         type: 'route',
         target: ['administration-server:0'],
         path: path,
-        route: 'aboutJahia',
+        route: routeId,
         defaultPath: path,
         icon: null,
         label: 'About Jahia',
