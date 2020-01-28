05b442a4eb5fe973d0e26ed3d75864c657a5e811
alex <alexandrk_@hotmail.com>
2020-01-28 10:01:21 -0500

    BACKLOG-12213 Remove react import

--- src/javascript/serverSettings/index.js
+++ src/javascript/serverSettings/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
 import {registerRoutes as registerAboutRoutes} from './aboutJahia/registerRoutes';
 import {registerRoutes as registerSystemHealthRoutes} from './systemHealth/registerRoutes';
 import {registerRoutes as registerConfigurationRoutes} from './configuration/registerRoutes';
 import {registerRoutes as registerSystemComponentsRoutes} from './systemComponents/registerRoutes';
 import {registerRoutes as registerUsersAndRolesRoutes} from './usersAndRoles/registerRoutes';
 import {registerRoutes as registerWebProjectsRoutes} from './webProjects/registerRoutes';
-import {useTranslation} from "react-i18next";
+import {useTranslation} from 'react-i18next';
 
-export default function() {
+export default function () {
     const {t} = useTranslation('serverSettings');
 
     registerAboutRoutes(t);
