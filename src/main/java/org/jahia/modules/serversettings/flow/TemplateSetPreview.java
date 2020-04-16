7b72d46004693d3628d437aa862d92d3b47ec5ca
Serge Huber <shuber@jahia.com>
2020-04-16 13:24:40 +0200

    BACKLOG-12656 new template selection (#35)
    * BACKLOG-12656 - New template selection component
    - Initial commit of new template selection component
    - Template sets must add images in their project in the images/template-preview directory for them to show here otherwise a placeholder will be used (PNG or GIF are supported)
    - Currently only the first image found is used.
    
    * BACKLOG-12656 - New template selection component
    - Code cleanup

---
+++ src/main/java/org/jahia/modules/serversettings/flow/TemplateSetPreview.java
@@ -0,0 +1,87 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2020 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.modules.serversettings.flow;
+
+import org.jahia.data.templates.JahiaTemplatesPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class contains information about template set preview resources used in the UI to present the template sets
+ * visually.
+ */
+public class TemplateSetPreview {
+
+    JahiaTemplatesPackage jahiaTemplatesPackage;
+    List<String> previewResources = new ArrayList<>();
+
+    public TemplateSetPreview() {
+    }
+
+    public TemplateSetPreview(JahiaTemplatesPackage jahiaTemplatesPackage, List<String> previewResources) {
+        this.jahiaTemplatesPackage = jahiaTemplatesPackage;
+        this.previewResources = previewResources;
+    }
+
+    public JahiaTemplatesPackage getJahiaTemplatesPackage() {
+        return jahiaTemplatesPackage;
+    }
+
+    public void setJahiaTemplatesPackage(JahiaTemplatesPackage jahiaTemplatesPackage) {
+        this.jahiaTemplatesPackage = jahiaTemplatesPackage;
+    }
+
+    public String getId() {
+        return jahiaTemplatesPackage.getId();
+    }
+
+    public List<String> getPreviewResources() {
+        return previewResources;
+    }
+
+    public void setPreviewResources(List<String> previewResources) {
+        this.previewResources = previewResources;
+    }
+}
