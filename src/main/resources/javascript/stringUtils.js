0f23daadbbf89e1e2df62c4192cf59850754a50a
Nico <nbugash@jahia.com>
2021-04-08 15:58:35 -0400

    QA-13522: added stringUtils.js to .eslintignore or else it will complain stringUtils.js is not used

--- src/main/resources/javascript/stringUtils.js
+++ src/main/resources/javascript/stringUtils.js
@@ -1,7 +1,6 @@
 function stringShortener(input) {
     if (input.length > 110) {
-        return input.substr(0,75) + "..." + input.substr(-29);
-    } else {
-        return input;
+        return input.substr(0, 75) + '...' + input.substr(-29);
     }
+    return input;
 }
