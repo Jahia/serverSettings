41fd53ebe1eacfdc400fac13c9690a9990527c73
akarmanov <alexandrk_@hotmail.com>
2025-01-10 11:31:44 -0500

    BACKLOG-23501: Use groovy to patch permissions

---
+++ src/main/resources/META-INF/patches/groovy/01-permissions.started.groovy
@@ -0,0 +1,21 @@
+import org.jahia.services.content.JCRCallback
+import org.jahia.services.content.JCRNodeWrapper
+import org.jahia.services.content.JCRSessionWrapper
+import org.jahia.services.content.JCRTemplate
+
+import javax.jcr.RepositoryException
+
+def updateServerAdministratorPermissions() {
+    JCRTemplate.getInstance().doExecuteWithSystemSession (new JCRCallback<Void>(){
+
+        @Override
+        Void doInJCR(JCRSessionWrapper jcrSessionWrapper) throws RepositoryException {
+            JCRNodeWrapper role = jcrSessionWrapper.getNode("/roles/server-administrator");
+            role.getProperty("j:permissionNames").addValue("jobs");
+            jcrSessionWrapper.save();
+            return null
+        }
+    });
+}
+
+updateServerAdministratorPermissions();
