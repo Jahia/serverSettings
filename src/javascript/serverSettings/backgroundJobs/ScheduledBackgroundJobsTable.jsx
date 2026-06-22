75ac91801a97b39867765ac580d9f230557cba8a
Félix Vallé <fvalle@jahia.com>
2026-06-22 11:47:41 +0200

    fix: use the right render format for moonstone 2.20.2

--- src/javascript/serverSettings/backgroundJobs/ScheduledBackgroundJobsTable.jsx
+++ src/javascript/serverSettings/backgroundJobs/ScheduledBackgroundJobsTable.jsx
@@ -1,6 +1,7 @@
 import React, {useMemo, forwardRef} from 'react';
 import {useTranslation} from 'react-i18next';
 import {useScheduledBackgroundJobs} from './hooks';
+import {stringColumn} from '@jahia/moonstone/DataTable';
 import BackgroundJobsTable from './BackgroundJobsTable';
 const ScheduledBackgroundJobsTable = forwardRef((_, ref) => {
     const {t} = useTranslation('serverSettings');
@@ -22,21 +23,18 @@ const ScheduledBackgroundJobsTable = forwardRef((_, ref) => {
                 key: 'name',
                 label: t('backgroundJobs.columns.name'),
                 width: '200px',
-                isSortable: true,
-                render: value => value
+                ...stringColumn(row => row.name)
             },
             {
                 key: 'jobDescription',
                 label: t('backgroundJobs.columns.jobDescription'),
-                isSortable: true,
-                render: value => value
+                ...stringColumn(row => row.jobDescription)
             },
             {
                 key: 'group',
                 label: t('backgroundJobs.columns.user'),
                 width: '150px',
-                isSortable: true,
-                render: value => value
+                ...stringColumn(row => row.group)
             }
         ];
     }, [t]);
